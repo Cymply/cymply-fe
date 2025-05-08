42a7c863ebbae6ca556bbece7e3434ac177d6754
susieSo <susie0529@imform.co.kr>
2025-05-08 17:53:03 +0900

    init: setup tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "../components/ui/button";
+
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen">
       <p>Hello World</p>
+      <Button>test</Button>
     </div>
   );
 }
