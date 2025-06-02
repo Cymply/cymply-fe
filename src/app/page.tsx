72f406f9bef53434352de9b9b9b1b07bc878a86c
bongjoonkim <bongjoonkim96@gmail.com>
2025-06-02 20:17:12 +0900

    기본 사용자 목록 보여주는 화면 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "../components/ui/button";
+import UserInfo from "@/app/admin/user/user-info";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <p>Hello World</p>
       <Button>test</Button>
+      <UserInfo />
     </div>
   );
 }
