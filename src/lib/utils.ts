8d77449917aa49fee21afbd30ad278367930ebe0
JaeHyun Bang <jhbang@plea.kr>
2025-06-24 10:40:07 +0900

    feat : isEmpty custom

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
 import { clsx, type ClassValue } from "clsx";
+import _ from "lodash";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function isEmpty(arg: unknown): boolean {
+  if (!arg) return true; // 0 = true 처리됨
+  if (typeof arg === "object") {
+    return _.isEmpty(arg);
+  }
+  return false;
 }
