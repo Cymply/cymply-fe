a3dea475c703c661779782047bd4d7ea7fd3aaff
devsuzy <suzy0529@gmail.com>
2025-07-06 01:03:01 +0900

    feat: 메인 페이지 퍼블리싱

--- src/widgets/header/ui/Logo.tsx
+++ src/widgets/header/ui/Logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export const Logo = () => {
   return (
-    <Link href="/" className="relative w-16 h-16 sm:w-24 sm:h-24">
+    <Link href="/" className="relative w-24 h-24">
       <Image src="/images/logo.svg" alt="logo" fill className="object-contain" />
     </Link>
   );
--- src/widgets/layout/ui/AppLayout.tsx
+++ src/widgets/layout/ui/AppLayout.tsx
@@ -3,9 +3,9 @@ import { Header } from "@/widgets/header";
 
 export const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="relative flex flex-col w-full h-screen">
+    <main className="relative flex flex-col w-full h-full bg-[url('/images/img-bg.png')] bg-cover bg-center bg-no-repeat">
       <Header />
-      <div className="flex flex-col items-center w-full pl-6 pr-6">{children}</div>
+      <div className="flex flex-col w-full h-full pl-9 pr-9">{children}</div>
     </main>
   );
 };
