0c2f9e8216c89dbfbc2ee7ad5638050585c836c5
devsuzy <suzy0529@gmail.com>
2025-08-25 00:16:32 +0900

    fix: 빌드 에러 수정

--- src/app/mypage/faq/page.tsx
+++ src/app/mypage/faq/page.tsx
@@ -1,26 +1,71 @@
-import { Suspense } from "react";
+"use client";
+import { Suspense, useEffect, useState } from "react";
+import { ExtendedRecordMap } from "notion-types";
 import { getData } from "@/shared/lib/notion";
 import { LoadingSpinner, Renderer } from "@/shared/ui";
+import { useSetAtom } from "jotai";
+import { alertAtom } from "@/widgets/alert";
 
 import "react-notion-x/src/styles.css";
 import "prismjs/themes/prism-tomorrow.css";
 import "katex/dist/katex.min.css";
 
-async function FaqPageContent() {
-  const pageId = "2458d05bbad3808084d1ffcc09dbdb5f";
-  const data = await getData(pageId);
+function FaqPageContent() {
+  const [data, setData] = useState<ExtendedRecordMap | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const setAlert = useSetAtom(alertAtom);
 
-  return (
-    <div className="flex-1">
-      <Renderer recordMap={data} rootPageId={pageId} />
-    </div>
-  );
+  const pageId = "2458d05bbad3808084d1ffcc09dbdb5f";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const notionData = await getData(pageId);
+        setData(notionData);
+      } catch (err) {
+        console.log(err);
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) return <LoadingSpinner />;
+
+  if (error) {
+    setAlert({
+      open: true,
+      title: (
+        <>
+          서버와의 연결에 <br />
+          문제가 생겼어요.
+        </>
+      ),
+      message: "인터넷 상태를 확인하거나,\n 잠시 후 다시 시도해 주세요.",
+      buttons: [
+        {
+          label: "홈으로 돌아가기",
+          action: () => {},
+        },
+      ],
+    });
+    return null;
+  }
+
+  return <Renderer recordMap={data} rootPageId={pageId} />;
 }
 
 export default function FaqPage() {
   return (
-    <Suspense fallback={<LoadingSpinner />}>
-      <FaqPageContent />
-    </Suspense>
+    <div className="flex-1">
+      <Suspense fallback={<LoadingSpinner />}>
+        <FaqPageContent />
+      </Suspense>
+    </div>
   );
 }
