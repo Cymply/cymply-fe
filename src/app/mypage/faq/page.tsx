a8b00a32ab7b193ef7a40c5dedecb2d8c71dda27
devsuzy <suzy0529@gmail.com>
2025-08-24 23:52:53 +0900

    fix: 빌드 에러 수정

--- src/app/mypage/faq/page.tsx
+++ src/app/mypage/faq/page.tsx
@@ -1,11 +1,12 @@
+import { Suspense } from "react";
 import { getData } from "@/shared/lib/notion";
-import { Renderer } from "@/shared/ui";
+import { LoadingSpinner, Renderer } from "@/shared/ui";
 
 import "react-notion-x/src/styles.css";
 import "prismjs/themes/prism-tomorrow.css";
 import "katex/dist/katex.min.css";
 
-export default async function FaqPage() {
+async function FaqPageContent() {
   const pageId = "2458d05bbad3808084d1ffcc09dbdb5f";
   const data = await getData(pageId);
 
@@ -13,5 +14,13 @@ export default async function FaqPage() {
     <div className="flex-1">
       <Renderer recordMap={data} rootPageId={pageId} />
     </div>
+  );
+}
+
+export default async function FaqPage() {
+  return (
+    <Suspense fallback={<LoadingSpinner />}>
+      <FaqPageContent />
+    </Suspense>
   );
 }
