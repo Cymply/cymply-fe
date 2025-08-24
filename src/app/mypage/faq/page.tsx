81788f7bbddfc17a58286d3de0ac38d51167bf87
devsuzy <suzy0529@gmail.com>
2025-08-24 23:37:09 +0900

    feat: Notion API로 노션 페이지 임베드

--- src/app/mypage/faq/page.tsx
+++ src/app/mypage/faq/page.tsx
@@ -1,15 +1,17 @@
-export default function FaqPage() {
-  const notionUrl =
-    "https://smart-bergamot-e66.notion.site/2458d05bbad3808084d1ffcc09dbdb5f?source=copy_link";
+import { getData } from "@/shared/lib/notion";
+import { Renderer } from "@/shared/ui";
+
+import "react-notion-x/src/styles.css";
+import "prismjs/themes/prism-tomorrow.css";
+import "katex/dist/katex.min.css";
+
+export default async function FaqPage() {
+  const pageId = "2458d05bbad3808084d1ffcc09dbdb5f";
+  const data = await getData(pageId);
 
   return (
     <div className="flex-1">
-      <iframe
-        src={notionUrl}
-        className="w-full h-full border-0"
-        title="자주 묻는 질문"
-        allowFullScreen
-      />
+      <Renderer recordMap={data} rootPageId={pageId} />
     </div>
   );
 }
