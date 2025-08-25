84a14b2b6a663decf553ae72dbeb69b62b492e37
JaeHyun Bang <jhbang@plea.kr>
2025-08-25 21:54:40 +0900

    fix : musicItem 이미지 경로 예외처리

--- src/shared/ui/musicItem.tsx
+++ src/shared/ui/musicItem.tsx
@@ -51,7 +51,7 @@ export const MusicItem = ({
       <div className="relative w-[7.5rem] min-w-[7.5rem] h-[7.5rem]">
         {music.thumbnail && (
           <Image
-            src={music.thumbnail || music.thumbnailUrl}
+            src={music.thumbnail || music.thumbnailUrl || ""}
             alt={music.title}
             fill
             className="object-contain rounded-[0.625rem]"
